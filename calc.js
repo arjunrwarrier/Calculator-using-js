c51553e147d366aec3edd4bf0ab7eec7035c1f7c
Ajun R Warrier <notwarrier@gmail.com>
2022-08-13 12:44:45 +0530

    Created calculator using html,css and javascript

---
+++ calc.js
@@ -0,0 +1,12 @@
+function buttonclick(val){
+document.getElementById("screen").value+=val;
+}
+function cleardisplay(){
+    document.getElementById("screen").value=""
+}
+function equalClick(){
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+   var disp= document.getElementById('screen').value+=("=")+result
+   
+}
